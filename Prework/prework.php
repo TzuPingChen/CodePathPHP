a9cf7de3060287fb90282ba61952765d97675926
TzuPingChen <TpChen82427@gmail.com>
2016-12-09 00:41:24 -0800

    Update prework.php

--- Prework/prework.php
+++ Prework/prework.php
@@ -13,6 +13,7 @@
     <input type="radio" name="tips" value="0.1">10%<br>
     <input type="radio" name="tips" value="0.15">15%<br>
     <input type="radio" name="tips" value="0.2">20%<br>
+    <input type="radio" name="tips" value="0">Tips(%):<input type="text" name="custom" value = "0"><br>
          
     <input type="submit" value="Calculate">
          
@@ -25,15 +26,28 @@ if(isset($_POST["Money"])){
     if(is_numeric($_POST["Money"])){
     	if(isset($_POST["tips"])){
 			$selectoption = $_POST["tips"];
-
 			$money = $_POST["Money"];
 
+            if($selectoption == 0){
+                if (is_numeric($_POST["custom"])&&$_POST["custom"]>=0){
+                    $selectoption = ($_POST["custom"]/100);
+                    $tipsonly = $money*$selectoption;
+                    $totalpayment = $money*($selectoption+1);
+                }else{
+                    echo '<p>'."Please enter a valid number of tip "."</p>\n";
+                    $tipsonly = "Error!(Custom tip should be numbers larger(equal) to 0)";
+                    $totalpayment = "Error!(Custom tip should be numbers larger(equal) to 0)";
+                }
+            }else{
 			$tipsonly = $money * $selectoption;
 			$totalpayment = $money * ($selectoption+1) ;
-			echo "Tips :".$tipsonly."__";
+            }
+            echo '<p>'."Your input Bill :".$_POST["Money"]."</p>\n";
+            echo '<p>'."Your tip % :".$selectoption."</p>\n";
+			echo '<p>'."Tips :".$tipsonly."</p>\n";
 			echo "Total payment :".$totalpayment;
 		}else echo "please select your tip option";	
-    }else echo "Please enter a valid number";
+    }else echo "Please enter a valid bill number";
 }else echo "please type your bill";
 ?>
 
